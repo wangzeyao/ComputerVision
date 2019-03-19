86ee5b9e88bd8a6eb5f5fd40cca07203e3577346
wangzeyao <blithetyrell@gmail.com>
2019-03-19 16:48:31 +0100

    修改camshift逻辑，加入readme
    人脸区域不再是固定的，而是会一直跟踪最新的位置。
    调整了截图的大小。

--- TP2/camshift.py
+++ TP2/camshift.py
@@ -28,35 +28,30 @@ import sys
 import getopt
 import numpy as np
 import cv2 as cv
-import pandas as pd
+import video
+from video import presets
 
 PY3 = sys.version_info[0] == 3
 
 if PY3:
     xrange = range
 
-# local module
-import video
-from video import presets
 
-
-class FaceDetect():
+class FaceDetect:
     def __init__(self):
         self.track_window = None
         self.cascade = None
 
-    def initialization(self):
+    def initialization(self):  # setup for using the haar classifer
         args, video_src = getopt.getopt(sys.argv[1:], '', ['cascade=', 'nested-cascade='])
         args = dict(args)
         cascade_fn = args.get('--cascade', "data/haarcascades/haarcascade_frontalface_alt.xml")
-        nested_fn = args.get('--nested-cascade', "data/haarcascades/haarcascade_eye.xml")
         self.cascade = cv.CascadeClassifier(cv.samples.findFile(cascade_fn))
-        nested = cv.CascadeClassifier(cv.samples.findFile(nested_fn))
 
-    def detect(self, img):
+    def detect(self, img):  # the function which get the rectangle of the detected face
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         gray = cv.equalizeHist(gray)
-        rects = self.cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30),
+        rects = self.cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30),
                                               flags=cv.CASCADE_SCALE_IMAGE)
         if len(rects) == 0:
             return []
@@ -66,19 +61,18 @@ class FaceDetect():
 
 class App(object):
     def __init__(self, video_src):
-        self.cam = video.create_capture(video_src, presets['cube'])
-        _ret, self.frame = self.cam.read()
+        self.cam = video.create_capture(video_src, presets[
+            'cube'])  # create_capture is a convenience function for capture creation,
+        _ret, self.frame = self.cam.read()  # capture a frame and store it
         cv.namedWindow('camshift')
-        # cv.setMouseCallback('camshift', self.onmouse)
 
         self.selection = None
-        # self.drag_start = None
         self.show_backproj = False
         self.face_find = False
         self.track_window = None
 
-    def show_hist(self):
-        bin_count = self.hist.shape[0]
+    def show_hist(self):  # show the plot Of histogram
+        bin_count = self.hist.shape[0]  # get the number of bins
         bin_w = 24
         img = np.zeros((256, bin_count * bin_w, 3), np.uint8)
         for i in xrange(bin_count):
@@ -88,35 +82,39 @@ class App(object):
         img = cv.cvtColor(img, cv.COLOR_HSV2BGR)
         cv.imshow('hist', img)
 
-    def run(self):
+    def run(self):  # main loop
+        face_detect = FaceDetect()  # i put the creation of the object and the initialization
+        # outside the loop to save time and memory
+        face_detect.initialization()
         while True:
             _ret, self.frame = self.cam.read()
-            img = self.cam.read()
             vis = self.frame.copy()
-            hsv = cv.cvtColor(self.frame, cv.COLOR_BGR2HSV)
-            mask = cv.inRange(hsv, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
-            if not self.selection and not self.face_find:
-                face_detect = FaceDetect()
-                face_detect.initialization()
-                rects = face_detect.detect(self.frame)
-                if len(rects):
-                    self.face_find = True
-                    self.selection = rects[0]
-                    xmin = min(self.selection[0], self.selection[2])
-                    xmax = max(self.selection[0], self.selection[2])
-                    ymin = min(self.selection[1], self.selection[3])
-                    ymax = max(self.selection[1], self.selection[3])
-                    self.selection = (xmin, ymin, xmax, ymax)
-                    self.track_window = (xmin, ymin, xmax - xmin, ymax - ymin)
-
-            if self.selection:
+            hsv = cv.cvtColor(self.frame, cv.COLOR_BGR2HSV)  # convert from BGR to HSV
+            mask = cv.inRange(hsv,
+                              np.array((0., 60., 32.)),  # lower bound
+                              np.array((180., 255., 255.)))  # upper bound
+            rects = face_detect.detect(self.frame)
+            if len(rects):  # face detected, set values of selection and track window
+                self.selection = rects[0]
+                xmin = min(self.selection[0], self.selection[2])
+                xmax = max(self.selection[0], self.selection[2])
+                ymin = min(self.selection[1], self.selection[3])
+                ymax = max(self.selection[1], self.selection[3])
+                self.selection = (xmin, ymin, xmax, ymax)
                 x0, y0, x1, y1 = self.selection
+
+            if self.selection and not self.face_find:  # we don't want to change the histogram after the fist detection
+                # of a face, so the code below loop will run only once
+                self.face_find = True
+                self.track_window = (xmin, ymin, xmax - xmin, ymax - ymin)
                 hsv_roi = hsv[y0:y1, x0:x1]
                 mask_roi = mask[y0:y1, x0:x1]
-                hist = cv.calcHist([hsv_roi],  # 原图
-                                   [0],  # channels，用于计算直方图的通道，灰度图是[1]，
+                hist = cv.calcHist([hsv_roi],  # the image
+                                   [0],  # channels used for calculate the histogram,[1] for gray scale.
+                                   # 用于计算直方图的通道，灰度图是[1]，
+                                   # colored image can use [0],[1],[2] to calculate blue, green and red
                                    # 彩色图可以用[0],[1],[2]来分别计算蓝，绿，红
-                                   mask_roi,  # 掩图，想找到特定区域的histogram使使用
+                                   mask_roi,  # mask 掩图，想找到特定区域的histogram时使用
                                    [16],  # hitsize BIN的数量，表示直方图分成多少分（即有多少个柱子）
                                    [0, 180])  # ??
                 cv.normalize(hist, hist, 0, 255, cv.NORM_MINMAX)
@@ -124,8 +122,8 @@ class App(object):
                 self.show_hist()
 
                 vis_roi = vis[y0:y1, x0:x1]
-                cv.bitwise_not(vis_roi, vis_roi)
-                vis[mask == 0] = 0
+                cv.bitwise_not(vis_roi, vis_roi)  # Inverts every bit of an array(why?)
+                vis[mask == 0] = 0  # wherever the value in mask equals 0, set 0 for vis.(why?)
 
             if self.track_window and self.track_window[2] > 0 and self.track_window[3] > 0:
                 self.selection = None
@@ -135,13 +133,12 @@ class App(object):
                                           [0, 180],  # Destination back projection array that is a single-channel
                                           #  array of the same size and depth as images
                                           1)
-                prob[y0:y1, x0:x1] = 0
-                prob &= mask
-                term_crit = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1)
+                prob[y0-30:y1+30, x0-10:x1+10] = 0  # set the probability of detected face to 0
+                prob &= mask  # 按位运算符
+                term_crit = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1)  # end criteria for camshift
                 track_box, self.track_window = cv.CamShift(prob,  # Back projection of the object histogram
                                                            self.track_window,  # Initial search window
                                                            term_crit)
-                trackwin = self.track_window
                 if self.show_backproj:
                     vis[:] = prob[..., np.newaxis]
                 try:
@@ -151,30 +148,23 @@ class App(object):
                     print(track_box)
 
             cv.imshow('camshift', vis)
-            # if cv.waitKey(10) == 32:
-            #     # letterC = prob[self.track_window[0]:self.track_window[1], self.track_window[2]:self.track_window[3]]
-            #     try:
-            #         cv.imwrite('hand.jpg',self.track_window)
-            #         print('save successful')
-            #     except:
-            #         print('save failed')
 
             ch = cv.waitKey(5)
             if ch == 27:
                 break
             if ch == ord('b'):
                 self.show_backproj = not self.show_backproj
-            if ch == 32:
-                # letterC = prob[self.track_window[0]:self.track_window[1], self.track_window[2]:self.track_window[3]]
+            if ch == ord('c'):
                 try:
-                    xs0,ys0,xs1,ys1 = self.track_window
-                    prob_hand = prob[ys0:ys1+ys0, xs0:xs1+xs0]
-                    color_hand = vis[ys0:ys1+ys0, xs0:xs1+xs0]
-                    # cv.imwrite('hand.png',vis[self.track_window[0]:self.track_window[1], self.track_window[2]:self.track_window[3]])
+                    xs0, ys0, xs1, ys1 = self.track_window
+                    prob_hand = prob[ys0:ys1 + ys0+20, xs0:xs1 + xs0+20]  # get back project image
+                    color_hand = vis[ys0:ys1 + ys0+20, xs0:xs1 + xs0+20]  # get normal image
                     cv.imwrite('D:/pythonProject/ComputerVision/TP2/files/handphoto/color.png', color_hand)
                     cv.imwrite('D:/pythonProject/ComputerVision/TP2/files/handphoto/prob.png', prob_hand)
-                    np.savetxt('D:/pythonProject/ComputerVision/TP2/files/csv/my.csv', prob_hand)
-                    print('save successful')
+                    prob_hand = cv.resize(prob_hand, dsize=(16, 16))  # resize it to 16x16
+                    np.savetxt('D:/pythonProject/ComputerVision/TP2/files/csv/training_set.txt', prob_hand)  # save the matrix of back project
+                    #  as .txt
+                    print('saved')
                 except:
                     print('save failed')
         cv.destroyAllWindows()
---
+++ TP2/readme.md
@@ -0,0 +1,27 @@
+﻿# CamShift
+
+### Aim
+The aim for this project is to track the hand in the camera and take a picture of it. We chose Camshift to track our object since it can adapt the size of the object. 
+### Problems
+But still we have two problmes to solve:
+* How do we get the color histogram (which is the color distribution of the object we want to track) of our hand?
+* How can we eliminate the effect of our face?
+
+### How to solve
+Fortunately, we can solve the two problems together by using the **Face detect**.
+
+ 1. Using face detect and get the histogram(hope that the color for our face and hands are similar), we use this histogram to find our hand
+ 2. Set the probability to zero in the area of face(see [back project](https://github.com/wangzeyao/ComputerVision/blob/master/Markdowns/%E5%8F%8D%E5%90%91%E6%8A%95%E5%BD%B1.md))
+
+So now the CamShift is responsible for the tracking(see also [meanshift and camshift](https://github.com/wangzeyao/ComputerVision/blob/master/Markdowns/MeanShift.md))  and the face detection find the face and wipe its probability of it, problem solved.
+
+### Result
+
+![colorful photo](https://lh3.googleusercontent.com/6HxHrem6aAD9bM8Zk2A43b5z9dFM8MF4_9QFZbzFZ9JEnbPemBy4QOeYSCcjK68ld1Hi1CgBwyQf "colorful")
+![enter image description here](https://lh3.googleusercontent.com/H3EuGk0ECrOhvjLbq4DKDU9XV6lOUndeTKMwqzHbEO4gyKYm_iVdlINsL77aSY7loC1343-2vzOd)
+**LOVE & PEACE**
+
+And the matrix which contain the probabiltiy after resized to 16X16![enter image description here](https://lh3.googleusercontent.com/aQelvcsRgM2wn7uPCuxZxa8l-eqPfAwmnhaLyKiGmLr9_HbllHoW8iDX7J1GScJvnd_1Az0CAUQn)
+It's for the next step: **Train a neural network to recogenize our hand gesture.**
+
+The code is in the same directory called `camshift.py` with comments.
